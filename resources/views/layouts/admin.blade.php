d3b475af710c0d7ed6349a45bbcab2b5f8bcda37
muhammadasadbastomi <tomycules@gmail.com>
2022-06-27 20:06:12 +0800

    CHANGE LOGO

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -597,10 +597,10 @@
         <aside class="sidebar-wrapper" data-simplebar="true">
             <div class="sidebar-header">
                 <div>
-                    <img src="{{ asset('assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon">
+                    {{-- <img src="{{ asset('assets/images/logo-icon.png') }}" class="logo-icon" alt="logo icon"> --}}
                 </div>
                 <div>
-                    <h4 class="logo-text">Skodash</h4>
+                    <h4 class="logo-text" style="color: red">HONDA</h4>
                 </div>
                 <div class="toggle-icon ms-auto"><i class="bi bi-chevron-double-left"></i>
                 </div>
